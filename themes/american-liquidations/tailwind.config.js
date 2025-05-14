2c945ad41a627817c0637996a872a45bd9237dd2
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2025-05-14 17:56:17 +0530

    Create tailwind.config.js

---
+++ themes/american-liquidations/tailwind.config.js
@@ -0,0 +1,45 @@
+/* @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./**/*.{php,html,js}", // Scan all subdirectories correctly
+    "./template-parts/**/*.{php,html,js}",
+    "./blocks/**/*.{php,html,js}",
+    "./woocommerce/**/*.{php,html,js}",
+    "./BicBlock/**/*.{php,html,js}"
+  ],
+  theme: {
+    extend: { 
+      fontFamily: {
+        Poppins: ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'primary': '#0083FF',
+        'green': '#D6FC4B',
+        'black': '#000000',
+        'body-color': '#000000',
+        'heading-color': '#000000',
+      },
+      fontSize: {
+        h1: '36px',
+        h2: '36px',
+        h3: '30px',
+        h4: '24px',
+        h5: '18px',
+        h6: '18px',
+        p: '18px',
+        'p-small': '14px',
+      },
+      container: {
+        center: true, // Keep the container centered
+        padding: '50px', // Set left & right padding to 50px
+        screens: {
+          DEFAULT: '1440px', // Force the container width to 124px
+        },
+      },
+
+      maxWidth:{
+      }
+    },
+  },
+  plugins: []
+};
