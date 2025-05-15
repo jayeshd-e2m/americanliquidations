d46953dbbdc6c512d2ee91b256a8974a07f8c166
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2025-05-15 21:27:51 +0530

    Update topbar.php

--- themes/american-liquidations/template-parts/partials/topbar.php
+++ themes/american-liquidations/template-parts/partials/topbar.php
@@ -17,15 +17,15 @@ $link_classes = 'text-white text-sm hover:underline ';
 <div aria-label="Top promotional links" class="bg-primary py-1.5">
     <div class="container">
         <div class="flex items-center justify-between">
-            <a href="#" class="text-white text-sm hover:underline ">Visit Our Pallet Outlet in Waterbury, CT!</a>
+            <a href="#" class="text-white text-sm hover:underline font-barlow">Visit Our Pallet Outlet in Waterbury, CT!</a>
 
             <span aria-hidden="true" class="mx-3 border-l border-white h-6 "></span>
 
-            <a href="#" class="text-white text-sm hover:underline ">Over 150 Pallets available</a>
+            <a href="#" class="text-white text-sm hover:underline font-barlow">Over 150 Pallets available</a>
 
             <span aria-hidden="true" class="mx-3 border-l border-white h-6 "></span>
 
-            <a href="#" class="text-white text-sm hover:underline ">Open to the Public 7 Days per week</a>
+            <a href="#" class="text-white text-sm hover:underline font-barlow">Open to the Public 7 Days per week</a>
 
             <div>
                 <a class="btn" href="">GET DIRECTIONS</a>
