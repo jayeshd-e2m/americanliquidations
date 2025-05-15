3e7a5b8ebfc1546caa7a1bf7bab3bff6311d13a3
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2025-05-15 21:20:46 +0530

    Update topbar.php

--- themes/american-liquidations/template-parts/partials/topbar.php
+++ themes/american-liquidations/template-parts/partials/topbar.php
@@ -11,23 +11,25 @@
 defined( 'ABSPATH' ) || exit;
 
 // Common link classes
-$link_classes = 'text-white text-sm hover:underline md:hidden';
+$link_classes = 'text-white text-sm hover:underline ';
 ?>
 
 <div aria-label="Top promotional links" class="bg-primary py-1.5">
-    <div class="container flex items-center justify-between ">
-        <a href="#" class="text-white text-sm hover:underline md:hidden">Visit Our Pallet Outlet in Waterbury, CT!</a>
+    <div class="container">
+        <div class="flex items-center justify-between">
+            <a href="#" class="text-white text-sm hover:underline ">Visit Our Pallet Outlet in Waterbury, CT!</a>
 
-        <span aria-hidden="true" class="mx-3 border-l border-white h-6 md:hidden"></span>
+            <span aria-hidden="true" class="mx-3 border-l border-white h-6 "></span>
 
-        <a href="#" class="text-white text-sm hover:underline md:hidden">Over 150 Pallets available</a>
+            <a href="#" class="text-white text-sm hover:underline ">Over 150 Pallets available</a>
 
-        <span aria-hidden="true" class="mx-3 border-l border-white h-6 md:hidden"></span>
+            <span aria-hidden="true" class="mx-3 border-l border-white h-6 "></span>
 
-        <a href="#" class="text-white text-sm hover:underline md:hidden">Open to the Public 7 Days per week</a>
+            <a href="#" class="text-white text-sm hover:underline ">Open to the Public 7 Days per week</a>
 
-        <div class="wp-block-button is-style-fill has-color-style-secondary has-arrow has-force_hover__outline_secondary md:mx-auto is-small-on-md">
-            <a class="wp-block-button__link wp-element-button " href="">GET DIRECTIONS</a>
+            <div class="wp-block-button is-style-fill has-color-style-secondary has-arrow has-force_hover__outline_secondary md:mx-auto is-small-on-md">
+                <a class="wp-block-button__link wp-element-button " href="">GET DIRECTIONS</a>
+            </div>
         </div>
     </div>
 </div>
