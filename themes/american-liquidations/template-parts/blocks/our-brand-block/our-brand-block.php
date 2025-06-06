04caef8e87b182074b4cd088497dd8f228e5061e
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2025-06-06 22:47:35 +0530

    php

--- themes/american-liquidations/template-parts/blocks/our-brand-block/our-brand-block.php
+++ themes/american-liquidations/template-parts/blocks/our-brand-block/our-brand-block.php
@@ -19,14 +19,14 @@ $block_id = get_field('advanced') ? get_field('block_id') : '';
 				if( have_rows('our_brand_blocks') ):
 					while( have_rows('our_brand_blocks') ) : the_row(); ?>
 						<div class="brand-item">
-							<div class="brand-item-img h-[148px] bg-gray flex items-center justify-center rounded-2xl p-3 mb-6">
+							<a href="<?php echo get_field('brand_url'); ?>" class="brand-item-img h-[148px] bg-gray flex items-center justify-center rounded-2xl p-3 mb-6">
 								<?php 
 								$image = get_sub_field('brand_image');
 								if( !empty( $image ) ): ?>
 									<img style="max-width: 90%; max-height: 90%;" src="<?php echo esc_url($image['url']); ?>" alt="<?php echo esc_attr($image['alt']); ?>" />
 								<?php endif; ?>
-							</div>
-							<h6 class="text-black/60"><?php echo get_sub_field('brand_title'); ?></h6>
+							</a>
+							<h6 class="text-black/60"><a href="<?php echo get_field('brand_url') ?>"><?php echo get_sub_field('brand_title'); ?></a></h6>
 							<p class="mt-5 leading-[1.2em]"><?php echo get_sub_field('brand_content'); ?></p>
 						</div>
 					<?php endwhile;
