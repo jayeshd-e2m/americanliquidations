ff6de614fc2217d0c48007f4d6afcd65a3edabea
chiragse2m <133650449+chiragse2m@users.noreply.github.com>
2025-06-24 17:03:36 +0530

    Css

--- themes/american-liquidations/template-parts/blocks/product-card.php
+++ themes/american-liquidations/template-parts/blocks/product-card.php
@@ -6,9 +6,9 @@ if (!is_a($product, 'WC_Product')) {
 global $product;
 ?>
 
-<div class="product-card rounded-[15px] bg-white relative overflow-hidden">
-
+<div class="product-card rounded-[15px] bg-white relative overflow-hidden flex flex-col h-full">
     <div class="product-card-img pt-[72%] bg-[#6988730D] rounded-[8px] md:rounded-[15px] relative overflow-hidden">
+
 		<?php if (!$product->is_in_stock()) {
 			echo '<span class="is-tag z-10 absolute top-2 left-2 out-of-stock bg-primary font-bold tracking-[0.06em] py-0.5 px-2 md:px-3 rounded-full md:text-[12px] text-[12px] !text-white uppercase inline-block align-top font-barlow">Out of Stock</span>';
 		}
@@ -23,8 +23,8 @@ global $product;
 		?>
     </div>
 
-    <div class="product-card-content pt-10 px-5 pb-7">
-    	<div class="product-card-text">
+    <div class="product-card-content pt-10 px-5 pb-7 flex flex-col flex-grow">
+    	<div class="product-card-text flex-grow">
 	        <h5 class="mb-4 is-title"><a href="<?php echo esc_url(get_permalink($product->get_id())); ?>"><?php echo esc_html( get_the_title( $product->get_id() ) ); ?></a></h5>
 			<div class="flex justify-between gap-2 flex-wrap">
 				<p class="text-[18px] text-black font-medium font-barlow flex items-center gap-2 is-price"><?php echo $product->get_price_html(); ?></p>
@@ -42,7 +42,7 @@ global $product;
 			</div>
 		</div>
 
-		<div class="flex items-center gap-5 mt-6 is-buttons flex-wrap ">
+		<div class="flex items-center gap-5 mt-6 is-buttons flex-wrap">
 			<?php $is_truckload = has_term( 'truckloads', 'product_cat', $product->get_id() ); ?>
 			<a href="<?php echo esc_url( $product->add_to_cart_url() ); ?>"
                class="add_to_cart_button custom-add-to-cart  relative"
