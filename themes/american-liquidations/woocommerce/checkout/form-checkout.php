5eb6469054958b191a2f84dd73428afc27fe62a4
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2025-11-20 11:37:26 +0530

    Update form-checkout.php

--- themes/american-liquidations/woocommerce/checkout/form-checkout.php
+++ themes/american-liquidations/woocommerce/checkout/form-checkout.php
@@ -75,11 +75,24 @@ defined( 'ABSPATH' ) || exit;
 										<span class="text-xs font-medium">Subtotal</span>
 										<span class="text-xs font-medium"><?php wc_cart_totals_subtotal_html(); ?></span>
 									</div>
-									<?php do_action( 'woocommerce_cart_totals_before_shipping' ); ?>
+									
+									<?php if ( WC()->cart->needs_shipping() && WC()->cart->show_shipping() ) : ?>
+
+										<?php do_action( 'woocommerce_cart_totals_before_shipping' ); ?>
 
 										<?php wc_cart_totals_shipping_html(); ?>
 
 										<?php do_action( 'woocommerce_cart_totals_after_shipping' ); ?>
+
+									<?php elseif ( WC()->cart->needs_shipping() && 'yes' === get_option( 'woocommerce_enable_shipping_calc' ) ) : ?>
+
+										<tr class="shipping">
+											<th><?php esc_html_e( 'Shipping', 'woocommerce' ); ?></th>
+											<td data-title="<?php esc_attr_e( 'Shipping', 'woocommerce' ); ?>"><?php woocommerce_shipping_calculator(); ?></td>
+										</tr>
+
+									<?php endif; ?>
+
 									<?php if ( WC()->cart->get_shipping_total() > 0 ) : ?>
 									<div class="flex justify-between mb-4">
 										<span class="text-xs font-medium">Shipping</span>
