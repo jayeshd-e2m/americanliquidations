55653c38445eaf8afd5a249e1253bffa519bcab7
jayeshd-e2m <jayeshd.patel@e2m.solutions>
2026-06-05 11:27:36 +0530

    Update footer.php

--- themes/american-liquidations/footer.php
+++ themes/american-liquidations/footer.php
@@ -242,9 +242,9 @@
 <div id="gf-popup-overlay" class="gf-popup-overlay">
 	<div class="gf-popup-box">
 		<button type="button" id="gf-popup-close" class="gf-popup-close" aria-label="Close">&times;</button>
-		<div class="gf-popup-content subscribe-cover">
+		<div class="gf-popup-content subscribe-cover text-center">
 			<h2 class="text-white mb-5">Subscribe to our <span>NEWSLETTER</span></h2>
-			<div class="max-w-[720px] mx-auto text-white/60">
+			<div class="max-w-[720px] mx-auto text-white/60 mb-5">
 				<p>Want to stay up to date on the liquidations market? Subscribe to our newsletter for free content!</p>
 			</div>
 			<?php echo do_shortcode('[gravityform id="3" title="false"]'); ?>
